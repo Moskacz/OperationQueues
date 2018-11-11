de7db5e0d2300a5fa7c4fbffa7f55a506880fb49
Michał Moskała <moskala.michal@gmail.com>
2018-11-11 20:25:37 +0100

    Created command line tool project from xcode template

---
+++ OperationQueues/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  OperationQueues
+//
+//  Created by Michal Moskala on 11/11/2018.
+//  Copyright © 2018 Michal Moskala. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
