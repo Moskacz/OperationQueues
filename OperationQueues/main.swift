b18d57c792265e3818e423b0b7de0806c084be6e
Michał Moskała <moskala.michal@gmail.com>
2018-11-11 21:04:01 +0100

    Added test

--- OperationQueues/main.swift
+++ OperationQueues/main.swift
@@ -8,4 +8,22 @@
 
 import Foundation
 
+enum Test {
+    case concurrentQueue
+    case serialQueue
+}
+
+var test = Test.serialQueue
+switch test {
+case .concurrentQueue:
+    let queue = OperationQueue()
+    queue.addOperation(SleepOperation(seconds: 2, name: "1"))
+    queue.addOperation(SleepOperation(seconds: 4, name: "2"))
+case .serialQueue:
+    let queue = OperationQueue()
+    queue.maxConcurrentOperationCount = 1
+    queue.addOperation(SleepOperation(seconds: 2, name: "1"))
+    queue.addOperation(SleepOperation(seconds: 4, name: "2"))
+}
+
 RunLoop.main.run()
